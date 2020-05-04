1e93fd7463f57158c60f5f50fc9bad6b734165ad
Diego Felix <dfelix@wkm.com.br>
2020-05-04 11:11:02 -0700

    Creating mocked version of networkManager

---
+++ src/mockedNetworkManager.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+import {fakeData as trips} from "./components/TripList/fakedata"
+import {fakeData as trip} from "./components/TripDetails/fakedata"
+
+export function getTrips() {
+    return new Promise((resolutionFunc, rejectionFunc) => {
+        resolutionFunc({data: trips});
+    });
+}
+
+export function getTrip(id) {
+    return new Promise((resolutionFunc, rejectionFunc) => {
+        resolutionFunc({data: trip});
+    });
+}
